d2b5bc8a6fbf0f85df191aec24941f47230a3ae9
John Fox <107318784+jff97@users.noreply.github.com>
2023-06-07 08:25:55 -0500

    module 4 done

--- WiredBrainCoffe.EmployeeManager/WiredBrainCoffe.EmployeeManager/Program.cs
+++ WiredBrainCoffe.EmployeeManager/WiredBrainCoffe.EmployeeManager/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddDbContext<EmployeeManagerDbContext>(
+builder.Services.AddDbContextFactory<EmployeeManagerDbContext>(
     opt => opt.UseSqlServer(
         builder.Configuration.GetConnectionString("EmployeeManagerDb")));
 
